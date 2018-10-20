2696f164e9b70c2c69c7dcda926684aad49bc37c
ca-evian <1485497679@qq.com>
2018-10-20 18:51:49 +0800

    temp

---
+++ transport.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public abstract class transport{
+    public String method;
+    public int getToStationTime;
+    public int returnFromStationTime;
+    public int ticketTime;
+    public int SecuritycheckIime;
+    public int waitingTime;
+    public int speed;
+    public int pricePerKil;
+
+    public abstract ArrayList<>(int);
+
+}
